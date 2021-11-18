6b29e873b280e9fcea5fab2309728d5715c6120b
Ольга Н. Курбатова <kon@2i.tusur.ru>
2021-11-18 13:08:56 +0700

    сохранение данных из формы в переменные

---
+++ send.php
@@ -0,0 +1,13 @@
+<?php
+$name = $_POST['full_name'];
+$country = $_POST['country'];
+$town = $_POST['town'];
+$education = $_POST['education'];
+$grade = $_POST['grade'];
+$form = $_POST['participation_form'];
+$email = $_POST['email'];
+$tel = $_POST['tel'];
+$subjects = $_POST['subject'];
+
+var_dump($_POST);
+echo ($subjects);
