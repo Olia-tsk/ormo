1e55d86541a52355c46f87c2c08e3195d06e7155
Ольга Н. Курбатова <kon@2i.tusur.ru>
2021-11-18 12:12:35 +0700

    открытие и закрытие модального окна

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  var modalButton = $("[data-toggle=modal]");
+  var modalCloseBtn = $(".modal-close");
+  var modalWindow = $(".modal");
+  var modalOverlay = $(".overlay");
+  var body = $("body");
+
+  // закрыть модальное окно на esc
+  $(document).on("keydown", function(e) {
+    if (e.keyCode == 27)
+    modalOverlay.removeClass("overlay--show");
+    modalWindow.removeClass("modal--active");
+    body.removeClass("overflow");
+  })
+
+  // закрыть модальное окно по щелчку вне окна
+  $(document).click(function(e) {
+    if (!modalButton.is(e.target) && modalWindow.is(e.target) && modalWindow.has(e.target).length === 0) 
+    {
+      modalOverlay.removeClass("overlay--show");
+      modalWindow.removeClass("modal--active");
+      body.removeClass("overflow");
+    }
+  })
+
+  function openModal() {
+    modalOverlay.addClass("overlay--show");
+    modalWindow.addClass("modal--active");
+    body.addClass("overflow");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    modalOverlay.removeClass("overlay--show");
+    modalWindow.removeClass("modal--active");
+    body.removeClass("overflow");
+  }
+
+  modalButton.on("click", openModal);
+  modalCloseBtn.on("click", closeModal);
+});
